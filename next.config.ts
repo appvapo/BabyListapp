59c30cd632d70e84901c51268a6ed8d19acb56e5
appvapo <benjamindelattre23@outlook.fr>
2025-08-31 01:11:42 +0200

    Fix Firebase API key configuration

--- next.config.ts
+++ next.config.ts
@@ -34,19 +34,6 @@ const nextConfig: NextConfig = {
     }
     return config;
   },
-   env: {
-    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
-    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-  },
-  devIndicators: {
-    allowedDevOrigins: [
-      'https://6000-firebase-studio-1756207006723.cluster-ikslh4rdsnbqsvu5nw3v4dqjj2.cloudworkstations.dev',
-    ],
-  }
 };
 
 export default nextConfig;
